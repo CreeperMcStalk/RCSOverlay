b09bdd59302547749062dfb90e155c523b55904e
Brendan Hagan <bhaganksu@gmail.com>
2017-05-06 22:49:50 -0400

    Switch to Vue

---
+++ resources/js/vue-main.js
@@ -0,0 +1,47 @@
+var POLL_INTERVAL = 2500;
+var JSON_PATH = JSON_PATH || './StreamControl_0_4b/streamcontrol.json';
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    info: {},
+    timestamp: new Date()
+  },
+  computed: {
+    formattedDate: function() {
+      return months[this.timestamp.getMonth() + 1] + ' ' +
+             this.timestamp.getDate() + ', ' +
+             this.timestamp.getFullYear();
+    },
+    formattedTime: function() {
+      return zeroPad(this.timestamp.getHours()) + ':' +
+             zeroPad(this.timestamp.getMinutes()) + ':' +
+             zeroPad(this.timestamp.getSeconds());
+    }
+  },
+  methods: {
+    loadJSON: function() {
+      axios.get(JSON_PATH, { responseType: 'json' })
+        .then(resp => { this.info = resp.data; })
+        .catch(resp => { console.error(resp); });
+    }
+  },
+  // Triggered when the vue instance is created, triggers the initial setup.
+  created: function() {
+    this.loadJSON();
+    setInterval(() => { this.loadJSON(); }, POLL_INTERVAL);
+    setInterval(() => { this.timestamp = new Date(); }, 1000);
+  }
+});
+
+
+/**
+ * Left Pad a Number to Ensure that it is two digits.
+ * @param  {int} number
+ * @return {String} Left padded result
+ */
+function zeroPad(number) {
+  return number < 10 ? '0' + number : '' + number;
+}
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
