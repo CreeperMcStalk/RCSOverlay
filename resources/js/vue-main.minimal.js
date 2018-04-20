ca96695bd5d9966f7ec3188d57b18060201a3c0d
Brandon Cooke <brandoncookedev@gmail.com>
2018-04-20 13:53:33 -0400

    Fixed vue transitions. JS failures caused hangups

---
+++ resources/js/vue-main.minimal.js
@@ -0,0 +1,117 @@
+/* DEPENDENCIES */
+/*
+if(!tunnel)
+    console.error('json-tunnel is required for vue-main. Please make sure you\'ve imported it');
+*/
+
+/** FILE SYSTEMS */
+const RESOURCES_DIR = '../resources'
+const IMAGES_DIR    = RESOURCES_DIR + '/images';
+const FONTS_DIR     = RESOURCES_DIR + '/fonts';
+const STYLES_DIR    = RESOURCES_DIR + '/styles';
+const VIDEOS_DIR    = RESOURCES_DIR + '/videos';
+
+var POLL_INTERVAL = 500;
+var ROUND_INTERVAL = 10000;
+var JSON_PATH = JSON_PATH || '../StreamControl_0_4b/streamcontrol.json';
+
+var port         = 11769;
+var smashGGinit  = 'http://localhost:'+port+'/init/';
+var smashGGround = 'http://localhost:'+port+'/getMatch';
+
+var currentTournament = '';
+
+/**
+ * Data Object to encapsulate player data
+ */
+class Player{
+    constructor(name, score, character, isOut){
+        this.name = name;
+        this.score = score;
+        this.character = character;
+        this.isOut = isOut;
+    }
+}
+
+/**
+ * Data Object for encapsulating Crew information
+ */
+class Crew{
+    constructor(name, players, score){
+        this.name = name;
+        this.players = players;
+        this.score = score;
+    }
+}
+
+/**
+ * Vue Application
+ */
+var app = new Vue({
+  el: '#app',
+  data: {
+    /* INFO OBJECT LINKS TO THE JSON CREATED BY STREAM CONTROL */
+    info: {
+        pull_mode: 'PULL_ALL',
+        event_countdown: 0,
+        event_notice: '',
+        event_name: '',
+        event_round: '',
+        best_of_x: '',
+
+        p1_name: '',
+        p2_name: '',
+
+        p1_games: '',
+        p2_games: '',
+
+        // Setting a few default values for the flicker of time images take to load.
+        p1_char: 'Default',
+        p2_char: 'Default',
+		
+		//URL for automated round pulling
+        smashggUrl: null
+        
+    },
+    timestamp: new Date()
+  },
+  watch: {
+  },
+  computed: {
+    formattedDate: function() {
+      return months[this.timestamp.getMonth() + 1] + ' ' +
+             this.timestamp.getDate() + ', ' +
+             this.timestamp.getFullYear();
+    },
+    formattedTime: function() {
+      return zeroPad(this.timestamp.getHours()) + ':' +
+             zeroPad(this.timestamp.getMinutes()) + ':' +
+             zeroPad(this.timestamp.getSeconds());
+    }
+  },
+  methods: {
+    loadJSON: function() {
+      axios.get(JSON_PATH, { responseType: 'json' })
+        .then(resp => { this.info = resp.data; })
+        .catch(resp => { console.error(resp); });
+    }
+  },
+  // Triggered when the vue instance is created, triggers the initial setup.
+  created: function() {
+    this.loadJSON();
+    setInterval(() => { this.timestamp = new Date(); }, 1000);
+    setInterval(this.loadJSON, POLL_INTERVAL);
+  }
+});
+
+
+/**
+ * Left Pad a Number to Ensure that it is two digits.
+ * @param  {int} number
+ * @return {String} Left padded result
+ */
+function zeroPad(number) {
+  return number < 10 ? '0' + number : '' + number;
+}
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
